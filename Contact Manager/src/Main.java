147747904ec1fbaad32e0f26867779b9455405c5
JiM2128 <60567866+JiM2128@users.noreply.github.com>
2023-06-23 13:46:30 +0300

    Main.java

--- Contact Manager/src/Main.java
+++ Contact Manager/src/Main.java
@@ -7,6 +7,10 @@ public class Main {
     }
 }
 
+// Log In details:
+// username: Admin
+// password: Password123
+
 
 // Compile the code in CMD with this command: 
 //----> javac -cp "lib/jackson-core-2.15.2.jar;lib/jackson-databind-2.15.2.jar;lib/jackson-annotations-2.15.2.jar" src/Contact.java src/ContactManager.java src/ContactManagerGUI.java src/LogIn.java src/SearchWindow.java src/EditWindow.java src/InputVarifier.java src/Main.java
